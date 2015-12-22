5aebedd5a66bba57873eabb9824b174150e2a10f
kejinlu <kejinlu@gmail.com>
2015-12-22 19:10:11 +0800

    Add comment for SwiftyTextLink

--- SwiftyText/SwiftyLabel/SwiftyTextLink.swift
+++ SwiftyText/SwiftyLabel/SwiftyTextLink.swift
@@ -11,10 +11,17 @@ import Foundation
 public let SwiftyTextLinkAttributeName: String = "SwiftyTextLink"
 
 /**
-  **SwiftyTextLink** holds link style information and identifier.
+  SwiftyTextLink holds link style and identifier infomation.
  
- Some special link identifier has been list as property such as URL, phoneNumber.
+ Some special link identifier has been list as property such as URL, phoneNumber etc..
  You also can use userInfo to identify you link.
+ 
+ **Properties**:
+ - attributes: Attributes which will be applied to the target text
+ - highlightedAttributes: Attributeds applied to the text when highlighted.
+ 
+ - highlightLayerRadius,highlightLayerColor: When highlight, there can be a mask layer over the link, these two properties can be setted to custom that layer
+ 
 */
 public class SwiftyTextLink: NSObject {
     public var attributes: [String: AnyObject]?
